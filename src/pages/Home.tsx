239438f86177c4bc9a5266a442934591bd00b217
nitin0778 <singh.ntn009@gmail.com>
2025-09-28 20:46:10 +0530

    image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -111,7 +111,7 @@ const Home: React.FC = () => {
             <p className="text-right font-semibold mt-2">- Rajesh Kumar</p>
           </div>
           <div className="bg-white p-4 rounded-xl shadow hover:shadow-xl transition">
-             <img src="" alt="girl"/>
+             <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="girl"/>
             <p className="text-gray-700 italic">“Booking tickets online was super easy and hassle-free. Highly recommended!”</p>
             <p className="text-right font-semibold mt-2">- Sita Sharma</p>
           </div>
